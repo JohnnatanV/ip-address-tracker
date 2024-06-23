04c5f279f14c0ea034b020cad44f3603e1f012a9
JohnnatanV <joh.villaneda@outlook.com>
2024-06-22 20:14:20 -0500

    Card container desing

--- src/Infotron.jsx
+++ src/Infotron.jsx
@@ -2,26 +2,27 @@ import "./Infotron.css";
 
 function Infotron() {
   return (
-    <ul>
-      <li>
-        <span>IP Address</span>
-      </li>
-      <li>
-        <span>Location</span>
-      </li>
-      <li>
-        <span>Timezone</span>
-      </li>
-      <li>
-        <span>UTC</span>
-      </li>
-      <li>
-        <span>ISP</span>
-      </li>
-      <li>
-        <span>ISP</span>
-      </li>
-    </ul>
+    <div className="container">
+      <ul>
+        <li className="card-container">
+          <span className="card-data">IP Address</span>
+          <span className="card-data-content">192.212.174.101</span>
+        </li>
+        <li className="card-container">
+          <span className="card-data">Location</span>
+          <span className="card-data-content">Brookllyn, NY</span>
+          <span className="card-data-content">10001</span>
+        </li>
+        <li className="card-container">
+          <span className="card-data">Timezone</span>
+          <span className="card-data-content">UTC -05:00</span>
+        </li>
+        <li className="card-container">
+          <span className="card-data">ISP</span>
+          <span className="card-data-content">SpaceX Starlink</span>
+        </li>
+      </ul>
+    </div>
   );
 }
 
